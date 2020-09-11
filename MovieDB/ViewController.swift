21a31d51013ff967bc19214c1efb778ec2d129da
Vladyslav Yermakov <vladyslav.yermakov@tbwamobile.com>
2020-09-11 12:44:44 +0300

    Initial Commit

---
+++ MovieDB/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieDB
+//
+//  Created by Ermac on 9/11/20.
+//  Copyright © 2020 Ermac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
